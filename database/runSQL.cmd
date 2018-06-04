6dd91e08ab8e7474cf8809ad7483eb8dd58fd342
Bingham <019631@aero.local>
2018-06-04 07:46:59 -0400

    updating the rest of the API methods and fixing the Dockerfile and run command

--- database/runSQL.cmd
+++ database/runSQL.cmd
@@ -1,1 +1,1 @@
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=mypassword' -e 'MSSQL_PID=Express' -p 1433:1433 -it --rm --name personapisql  microsoft/mssql-server-linux:latest
+docker run -d -p 1433:1433 --rm --name personapi-db personapi-db
--- peopleapi/Controllers/PeopleController.cs
+++ peopleapi/Controllers/PeopleController.cs
@@ -39,20 +39,66 @@ namespace peopleapi.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]Person value)
-        {
+        public async Task<IActionResult> Post([FromBody]Person value)
+        {            
+            if (value == null) {
+                return BadRequest();
+            }
+
+            Person p = value;
+            p.PersonId = Guid.NewGuid();
+            _context.Person.Add(p);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute(new {id = p.PersonId}, p);
         }
 
         // PUT api/values/GUID-TO-PERSON
-        [HttpPut("{id}")]
-        public void Put(Guid id, [FromBody]Person value)
+        [HttpPut]
+        public async Task<IActionResult> Put(Guid id, [FromBody]Person value)
         {
+            if (value.PersonId == null) {
+                return BadRequest();
+            }
+            
+            var person = await _context.Person.SingleOrDefaultAsync(x => x.PersonId == value.PersonId);
+            if (person == null) {
+                return NotFound();
+            }
+            // update the data
+            person.FirstName = value.FirstName;
+            person.MiddleName = value.MiddleName;
+            person.LastName = value.LastName;
+            person.Address = value.Address;
+            person.CellPhone = value.CellPhone;
+            person.City = value.City;
+            person.Email = value.Email;
+            person.Linkedin = value.Linkedin;
+            person.State = value.State;
+            person.Twitter = value.Twitter;
+            person.WorkPhone = value.WorkPhone;
+            person.ZipCode = value.ZipCode;
+            // save the data
+            _context.Person.Update(person);
+            await _context.SaveChangesAsync();
+
+            return new OkResult();
         }
 
         // DELETE api/values/GUID-TO-PERSON
         [HttpDelete("{id}")]
-        public void Delete(Guid id)
+        public async Task<IActionResult> Delete(Guid id)
         {
+             if (id == null) {
+                    return BadRequest();
+            }
+            var p = await _context.Person.SingleOrDefaultAsync(x => x.PersonId == id);
+            if (p == null) {
+                return NotFound();
+            }
+            _context.Person.Remove(p);
+            await _context.SaveChangesAsync();
+            return new OkResult();
         }
     }
 }
